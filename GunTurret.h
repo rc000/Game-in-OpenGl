dd820edd011ec6e0489db54a07f72f09942cdea4
rc000 <rc000@wp.pl>
2018-07-05 08:21:30 +0200

    Update and rename Wieza.h to GunTurret.h

--- GunTurret.h
+++ GunTurret.h
@@ -1,13 +1,13 @@
 #pragma once
  #include "stdafx.h"
 
-class Wieza {
+class GunTurret {
 	float x, x1, x2, y, z, z1, z2;
 	int trafienia;
 	GLuint wieza;
 public:
 	void set(float xt, float yt, float zt, GLuint wieza);
-	Wieza();
+	GunTurret();
 	void Render(float move_op, float posy, float posz, float rot_start, float rot, GLuint wieza,float rot_wreck);
 	bool Kolizja(float posx, float z);
 };
