19895d5e5ee15ac0011962c21ee989660f94da08
rc000 <rc000@wp.pl>
2018-07-05 08:19:35 +0200

    Update and rename Kadlub.h to Core.h

--- Core.h
+++ Core.h
@@ -1,12 +1,12 @@
 #pragma once
 #include "stdafx.h"
 
-class Kadlub {
+class Core {
 	float x, x1, x2, y, z, z1, z2;
 	int trafienia;
 	GLuint kadlub;
 public:
 	void set(float xt, float yt, float zt, GLuint kadlub);
-	Kadlub();
+	Core();
 	void Render(float move_op, float posy, float posz,float rot_start, float rot, GLuint kadlub);
 };
